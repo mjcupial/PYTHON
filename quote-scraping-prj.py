779ec9c54d5b681fff24bfe22cc2c1a1bf1bb464
cupial <mj.cupial@gmail.com>
2022-10-28 11:14:11 +0200

    next_page scrapped and .gitignore added

--- quote-scraping-prj.py
+++ quote-scraping-prj.py
@@ -7,12 +7,13 @@ from bs4 import BeautifulSoup
 
 all_quotes = []
 res = requests.get("http://quotes.toscrape.com")
-soup = BeautifulSoup(res.text)
+soup = BeautifulSoup(res.text, "html.parser")
 quotes = soup.find_all(class_="quote")
 for quote in quotes:
-    # print(quote.find(class_="text").get_text)
     all_quotes.append({
         "text": quote.find(class_="text").get_text(),
-        "author": quote.find(class_="author").get_text()
+        "author": quote.find(class_="author").get_text(),
+        "bio": quote.find("a")["href"]
     })
-print(all_quotes)
+next_page = soup.find(class_="next")
+print(next_page.find("a")["href"])
