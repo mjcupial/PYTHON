0953506be1cac406cf40600ed73a41f72d83639b
cupial <mj.cupial@gmail.com>
2022-10-28 10:47:02 +0200

    dict with text and author added inside all_quotes list

--- quote-scraping-prj.py
+++ quote-scraping-prj.py
@@ -5,8 +5,14 @@ http://quotes.toscrape.com
 import requests
 from bs4 import BeautifulSoup
 
+all_quotes = []
 res = requests.get("http://quotes.toscrape.com")
 soup = BeautifulSoup(res.text)
 quotes = soup.find_all(class_="quote")
 for quote in quotes:
-    print(quote.find(class_="text").get_text)
+    # print(quote.find(class_="text").get_text)
+    all_quotes.append({
+        "text": quote.find(class_="text").get_text(),
+        "author": quote.find(class_="author").get_text()
+    })
+print(all_quotes)
