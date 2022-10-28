33da7d7397d74de8bc5483988ab505bdccdeb645
cupial <mj.cupial@gmail.com>
2022-10-28 10:39:13 +0200

    get text as quote from class quote

--- quote-scraping-prj.py
+++ quote-scraping-prj.py
@@ -2,3 +2,11 @@
 
 http://quotes.toscrape.com
 '''
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get("http://quotes.toscrape.com")
+soup = BeautifulSoup(res.text)
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    print(quote.find(class_="text").get_text)
