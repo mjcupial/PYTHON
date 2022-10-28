2d5fceb82033da776f902b0e715ad648368a0e16
cupial <mj.cupial@gmail.com>
2022-10-28 14:15:30 +0200

    return all_q in wrong place

--- quote-scraping-prj.py
+++ quote-scraping-prj.py
@@ -8,10 +8,9 @@ from time import sleep
 from random import choice
 
 BASE_URL = "http://quotes.toscrape.com"
-
 def scrape_quotes():
     all_quotes = []
-    url = "/page/1/"
+    url = "/page/1"
     while url:
         res = requests.get(f"{BASE_URL}{url}")
         print(f"Scraping {BASE_URL}{url}...")
@@ -26,7 +25,7 @@ def scrape_quotes():
         next_btn = soup.find(class_="next")
         url = next_btn.find("a")["href"] if next_btn else None
         sleep(2)    #--> do not overload the server in each requests, be polite and set sleep
-        return all_quotes
+    return all_quotes
 def start_game(quotes):
     quote = choice(quotes)
     remaining_guesses = 4
