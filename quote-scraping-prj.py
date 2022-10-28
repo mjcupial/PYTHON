9ab9657649aa2d495aaf76f93432085e8b417743
cupial <mj.cupial@gmail.com>
2022-10-28 12:30:33 +0200

    all tips implemented

--- quote-scraping-prj.py
+++ quote-scraping-prj.py
@@ -30,17 +30,24 @@ while url:
 quote = choice(all_quotes)
 remaining_guesses = 4
 print("Here is a quote: " + quote["text"])
-# print(quote["text"] + quote["author"]) #-->  hint
+print(quote["text"] + quote["author"]) #-->  hint
 guess = ''
 while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
-    guess = input(f"Who said this quote? Guess remaining: {remaining_guesses}")
+    guess = input(f"Who said this quote? Guess remaining: {remaining_guesses}  -> ")
     remaining_guesses -= 1
     if remaining_guesses == 3:
         res = requests.get(f"{base_url}{quote['bio']}")
         soup = BeautifulSoup(res.text, "html.parser")
         birth_date = soup.find(class_="author-born-date").get_text()
         birth_place = soup.find(class_="author-born-location").get_text()
-        print(f"Here's a hint:\nThe author was born in {birth_place} in {birth_date}")
+        print(f"Here's a hint:\nThe author was born {birth_place} | {birth_date}")
+    elif remaining_guesses == 2:
+        print(f"Here's a hint: The author's first name starts with: {quote['author'][0]}")
+    elif remaining_guesses == 1:
+        last_initial = quote["author"].split(" ")[1][0]
+        print(f"Here's a hint: The author's last name starts with: {last_initial}")
+
+
 
 
 
