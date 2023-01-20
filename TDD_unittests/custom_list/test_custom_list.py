90f4e4175a4c65e9e9b6519b2895bd66eb48125c
cupial <mj.cupial@gmail.com>
2023-01-20 13:36:26 +0100

    func in func example

--- TDD_unittests/custom_list/test_custom_list.py
+++ TDD_unittests/custom_list/test_custom_list.py
@@ -8,5 +8,9 @@ class MyFirstTest(unittest.TestCase):
     def test_customfunc_x(self):
         self.assertEqual(custom_func_x(3,2,3), 54)
 
+    def test_custom_func_x(self):
+        self.assertEqual(custom_non_lin_list(5,2,3)[2], 16)
+        self.assertEqual(custom_non_lin_list(5, 3, 2)[4], 48)
+
 if __name__ == '__main__':
     unittest.main()
