dfe000a1c3694fbcbbacdef54021f3519c71c801
cupial <mj.cupial@gmail.com>
2023-02-10 22:06:07 +0100

    tamagotchi/tamagochi.py

--- tamagotchi/tamagochi.py
+++ tamagotchi/tamagochi.py
@@ -27,18 +27,16 @@ class Animal:
             print("I can't drink, there is no enough water!")
 
     def eat(self):
-        how_much = randint(5, 12)
+        how_much = randint(5, 22)
         Animal.food_in_bowl -= how_much
         is_poop = random.choice([False, True])
         if is_poop == True:
-            how_much = randint(1,10)
+            how_much = randint(1,22)
             Animal.poop += how_much
         if Animal.food_in_bowl > 0:
             print(f"Delicious! food in bowl: {Animal.food_in_bowl}%")
         else:
             print("I can't eat, there is no enough portion!")
-
-
 
     def add_water(self):
         how_much = randint(5, 68)
@@ -72,41 +70,28 @@ class Cat(Animal):
 
 
     def check_litter_box(self):
-        return self.poop
+        if Animal.poop >= 20:
+            print(f"It's time to clean litter box up, don't you think?")
+            decision = input("y/n: ")
+            if decision == 'y' or 'Y':
+                Animal.poop = 0
+        else:
+            print(f"Litter box: {Animal.poop}%")
 
 
-
-# class Mouse(Animal, Cat):
-#     def __init__(self, name, age, toy, is_kuweta_clear):
-#         super().__init__(name, age, is_predator=True)
-#         self.toy = toy
-#         self.is_kuweta_clear = is_kuweta_clear
-
-
-
-
-
-
-# animal = Animal("Piesel", "4", True)
-# print(animal)
-# animal.drink_water()
-# animal.drink_water()
-# print(Animal.water_in_bowl)
-# animal.add()
 #############################################################
-cat = Cat("Kicia", "8", True)
+cat = Cat("Kitty", "8", True)
 print(cat)
 cat.eat()
-print(cat.poop)
-
 cat.eat()
-print(cat.poop)
-
 cat.eat()
-print(cat.poop)
-
 cat.eat()
-print(cat.poop)
+
+cat.check_litter_box()
+cat.eat()
+cat.eat()
+cat.check_litter_box()
+
 
 
 
